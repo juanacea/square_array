98f4b741b7d1e2f6a63774915a7220707af13371
juanacea <juan.acea93@gmail.com>
2021-01-24 21:32:27 -0500

    commit tree e93ec4953f2e0018e99ba4cb21ad6162c0a6fc41

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  array.each do |number|
+    new_number = number**2
+    array[counter] = new_number
+    counter += 1
+  end
+  return array
 end
